b51cdbdb67126b334ba706ce032de0be8942fd62
Anonymous Researcher <anonymous@example.com>
2025-12-17 15:22:22 +0800

    fix infer_single.sh

--- bash_scripts/infer_single.sh
+++ bash_scripts/infer_single.sh
@@ -1,8 +1,8 @@
-AUDIO_PATH="inference/example/Ym8O802VvJes.wav"
+AUDIO_PATH="example/Ym8O802VvJes.wav"
 CAPTION="Mix in dog barking in the middle."
 
 
-CUDA_VISIBLE_DEVICES=0 python infer.py \
+CUDA_VISIBLE_DEVICES=1 python infer.py \
     +audio_path="$AUDIO_PATH" \
     +caption="$CAPTION" \
     +ckpt_dir=ckpt/mmedit \
